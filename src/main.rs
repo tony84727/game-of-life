e499b08025407c6f6fa27f88ae619e9ea1cebba7
tony84727 <tony84727@gmail.com>
2020-04-22 21:56:41 +0800

    read display config from file

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use amethyst::{
     prelude::*,
     renderer::{types::DefaultBackend, RenderFlat2D, RenderToWindow, RenderingBundle},
     utils::application_root_dir,
-    window::DisplayConfig,
     LoggerConfig, Result,
 };
 
@@ -14,24 +13,13 @@ fn main() -> Result<()> {
     amethyst::start_logger(LoggerConfig::default());
     let app_dir = application_root_dir()?;
     let asset_dir = app_dir.join("assets");
+    let config_dir = app_dir.join("config");
     let game_data = GameDataBuilder::default().with_bundle(
         RenderingBundle::<DefaultBackend>::new()
-            .with_plugin(RenderToWindow::from_config(DisplayConfig {
-                title: String::from("Game of Life"),
-                fullscreen: None,
-                dimensions: Some((500, 500)),
-                min_dimensions: None,
-                max_dimensions: None,
-                visibility: true,
-                icon: None,
-                loaded_icon: None,
-                always_on_top: false,
-                decorations: true,
-                maximized: false,
-                multitouch: false,
-                resizable: true,
-                transparent: false,
-            }))
+            .with_plugin(
+                RenderToWindow::from_config_path(config_dir.join("display.ron"))?
+                    .with_clear([0.0, 0.0, 0.0, 0.0]),
+            )
             .with_plugin(RenderFlat2D::default()),
     )?;
     let mut game = Application::new(asset_dir, GameState, game_data)?;
